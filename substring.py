1f777ccc7c93158c3431bae790e40b6280a8b4e9
valtams <markus.valtonen@student.lab.fi>
2025-09-11 19:56:41 +0300

    Substring basics

---
+++ substring.py
@@ -0,0 +1,27 @@
+#Counting starts from 0
+sentance = "0123456789abcdef"
+print(sentance)
+
+#1st
+print(sentance[0])
+
+#2nd to last
+print(sentance[-2])
+
+#from 3rd to 9th
+print(sentance[2:9])
+
+#first 9
+print(sentance[:9])
+
+#from -9th to -2nd
+print(sentance[-9:-2])
+
+#every third starting from 3rd to 9th
+print(sentance[2:9:3])
+
+#every third
+print(sentance[::3])
+
+#reverse
+print(sentance[::-1])
