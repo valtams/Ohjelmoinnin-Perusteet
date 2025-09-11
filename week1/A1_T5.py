fd67a429a63a2ed43729213e4ae2f3019ce63833
valtams <markus.valtonen@student.lab.fi>
2025-09-11 19:56:13 +0300

    How I first made it

--- week1/A1_T5.py
+++ week1/A1_T5.py
@@ -1,9 +1,5 @@
 print("Calculate the area of a wall.")
-Width = input("Enter the width in meters: ").replace(",",".")
-Height = input("Enter the height in meters: ").replace(",",".")
-Area = float(Width) * float(Height)
-print(f"Width is {Width} m and height is {Height} m.")
-if Area % 1 == 0:
-        Area = int(Area)
-else: Area = round(Area, 2)
-print(f"The wall will be {Area} square meters.")
+W = float(input("Enter the width in meters: ").replace(",","."))
+H = float(input("Enter the height in meters: ").replace(",","."))
+print(f"Width is {W}m and height is {H}m.")
+print(f"The wall will be {round(float(W) * float(H), 2)} square meters.")
