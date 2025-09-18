923a7c927ce27580bf8911bc1da9d3961648e54a
valtams <markus.valtonen@student.lab.fi>
2025-09-18 22:03:15 +0300

    Add missing parts to output

--- week2/Task6.py
+++ week2/Task6.py
@@ -6,6 +6,6 @@ if len(Input) == 3 or len(Input) == 6:
 	R = Input[0:2]
 	G = Input[2:4]
 	B = Input[4:6]
-	print(f"\nColors\n- {R}\n- {G}\n- {B}\n")
+	print(f"\nColors\n- Red {R}\n- Green {G}\n- Blue {B}\n")
 else: print("Invalid hex color.")
 print("Program ending.")
