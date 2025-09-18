7c9a48e0bb74eeb503f72f4e9223252047320300
valtams <markus.valtonen@student.lab.fi>
2025-09-18 11:49:15 +0300

    Add mpg as an option

--- week1/A1_T7.py
+++ week1/A1_T7.py
@@ -1,5 +1,13 @@
 print("Calculate fuel consumption.")
-Distance = int(input("Enter travel distance (kilometers): "))
-Feed = input("Enter fuel usage (liters): ")
-Consumption = int(Feed) / (Distance / 100)
-print(f"Fuel consumption is {round(Consumption, 2)}l/100km")
+Feed = input("Enter travel distance (Include units): ")
+Units = Feed[-2:]
+Distance = int(Feed.replace(Units,""))
+if Units == "mi":
+	Usage = int(input("Enter fuel usage (gallons): "))
+	Consumption = Distance / Usage
+	print(f"Fuel consumption is {round(Consumption, 2)}mpg")
+elif Units == "km":
+	Usage = int(input("Enter fuel usage (liters): "))
+	Consumption = Usage / (Distance / 100)
+	print(f"Fuel consumption is {round(Consumption, 2)}l/100km")
+else: print("Only l/100km and mpg are supported")
