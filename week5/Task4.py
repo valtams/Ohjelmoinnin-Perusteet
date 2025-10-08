9c05573ecf39ca6f7a7fa056c07a2f3bf28d3d5b
valtams <markus.valtonen@student.lab.fi>
2025-10-09 00:24:13 +0300

    read through the instructions again

--- week5/Task4.py
+++ week5/Task4.py
@@ -1,13 +1,13 @@
 def askDimension(PPrompt: str) -> float:
-   Feed = input(f"Insert {PPrompt}: ")
-   return float(Feed)
+   Feed = float(input(f"Insert {PPrompt}: "))
+   return Feed
 
 Width = askDimension("width")
 Height = askDimension("height")
 
 def calcRectangleArea(PWidth: float, PHeight: float) -> float:
-   Area = PWidth * PHeight
-   return Area
+   Sum = PWidth * PHeight
+   return Sum
 
 Area = calcRectangleArea(Width, Height)
 print("Program starting.")
