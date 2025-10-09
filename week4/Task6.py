d8411d0c4276a6c81f2441526eb340e70aab4d96
valtams <markus.valtonen@student.lab.fi>
2025-10-09 13:55:47 +0300

    Fix broken check for negative values

--- week4/Task6.py
+++ week4/Task6.py
@@ -1,14 +1,15 @@
 print("Program starting.")
 i = int(input("Insert a positive integer: "))
 c = 0
-if i <= 0:
-	print("Only positive integers are allowed.")
-while i != 1:
+while i != 1 and i > 0:
 	c += 1
 	print(i, end=" -> ")
 	if i % 2 == 1:
 		i = i * 3 + 1
 	else: i //= 2
-print(i)
-print(f"Sequence {c} total steps.")
+if i <= 0:
+	print("Only positive integers are allowed.")
+else:
+	print(i)
+	print(f"Sequence had {c} total steps.")
 print("\nProgram ending.")
